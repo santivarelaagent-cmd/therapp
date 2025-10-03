07e0a3851a0d00c81af852159b152fa4f0e455b6
santiago <svarela03@uan.edu.co>
2025-10-03 10:45:42 -0500

    cleaned

--- app/src/main/java/com/example/therapp/ui/presenter/pose_camera/PoseOverlay.kt
+++
@@ -1,9 +0,0 @@
-package com.example.therapp.ui.presenter.pose_camera
-
-/**
- * @author Santiago Varela Daza
- * @email svarela03@uan.edu.co
- * @github https://github.com/sanvarela03
- * @since 7/29/2025
- * @version 1.0
- */
